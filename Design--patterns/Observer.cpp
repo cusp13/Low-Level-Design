1a351e6cf818d0dec0f3a56910ac54fbf6773126
Divyansh Sharma <108236157+cusp13@users.noreply.github.com>
2025-07-26 10:55:11 +0530

    Create Observer.cpp
    Adding observer design pattern

---
+++ Design--patterns/Observer.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+// Interface for Subscriber
+class ISubscriber {
+public:
+    virtual void update(const string& videoTitle) = 0;
+    virtual ~ISubscriber() {};
+};
+
+// Interface for Channel
+class IChannel {
+public:
+    virtual void subscribe(ISubscriber* subscriber) = 0;
+    virtual void unsubscribe(ISubscriber* subscriber) = 0;
+    virtual void notifySubscribers(const string& videoTitle) = 0;
+    virtual ~IChannel() {};
+};
+
+// Concrete Channel
+class Channel : public IChannel {
+    string name;
+    vector<ISubscriber*> subscribers;
+
+public:
+    Channel(const string& name) {
+        this->name = name;
+    }
+
+    void subscribe(ISubscriber* subscriber) override {
+        if (find(subscribers.begin(), subscribers.end(), subscriber) == subscribers.end()) {
+            subscribers.push_back(subscriber);
+        }
+    }
+
+    void unsubscribe(ISubscriber* subscriber) override {
+        auto it = find(subscribers.begin(), subscribers.end(), subscriber);
+        if (it != subscribers.end()) {
+            subscribers.erase(it);
+        }
+    }
+
+    void notifySubscribers(const string& videoTitle) override {
+        for (ISubscriber* subscriber : subscribers) {
+            subscriber->update(videoTitle);
+        }
+    }
+
+    void uploadVideo(const string& videoTitle) {
+        cout << "Channel " << name << " uploaded a new video: " << videoTitle << endl;
+        notifySubscribers(videoTitle);
+    }
+};
+
+// Concrete Subscriber
+class User : public ISubscriber {
+    string username;
+public:
+    User(const string& name) {
+        username = name;
+    }
+
+    void update(const string& videoTitle) override {
+        cout << username << " got notified about: " << videoTitle << endl;
+    }
+};
+
+// Driver
+int main() {
+    Channel techChannel("TechVision");
+
+    User alice("Alice");
+    User bob("Bob");
+
+    techChannel.subscribe(&alice);
+    techChannel.subscribe(&bob);
+
+    techChannel.uploadVideo("Large Cap fund Video");
+
+    techChannel.unsubscribe(&bob);
+
+    techChannel.uploadVideo("Mid Cap fund Video");
+
+    return 0;
+}
