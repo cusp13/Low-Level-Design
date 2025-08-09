c05d0ecca5a4ac0fc119a38b3783bf75133da306
divyansh sharma <divyanshsharma@divyanshs-MacBook-Air.local>
2025-08-09 13:52:30 +0530

    Adding device manager which follows singleton design pattern

--- MusicPlayer/managers/DeviceManager.hpp
+++ MusicPlayer/managers/DeviceManager.hpp
@@ -2,4 +2,49 @@
 #include<iostream>
 #include "../devices/IAudioOutputDevice.hpp";
 #include "../enums/DeviceType.hpp";
+#include "../factories/DeviceFactory.hpp";
 
+using namespace std;
+
+class DeviceManager {
+private: 
+    static DeviceManager* instance;
+    IAudioOutputDevice* currentOutputDevice;
+    DeviceManager() {
+        currentOutputDevice = nullptr;
+    }
+public:
+    static DeviceManager* getInstance(){
+        if(instance == nullptr){
+            instance = new DeviceManager();
+        }
+    }
+    void connect(DeviceType deviceType){
+        if(currentOutputDevice){
+            delete currentOutputDevice;
+        }
+        currentOutputDevice = DeviceFactory::createDevice(deviceType);
+
+        switch(deviceType){
+            case DeviceType::BLUETOOTH:
+            cout<<"Bluetooth Device connected\n";
+            break;
+            case DeviceType::HEADPHONES:
+            cout<<"Headphone Device connected\n";
+            break;
+            case DeviceType::WIRED:
+            cout<<"Wired Device connected\n";
+        }
+    }
+    IAudioOutputDevice* getOutputDevice() {
+        if(!currentOutputDevice){
+            throw runtime_error("No device connected");
+        }
+        return currentOutputDevice;
+    }
+    bool hasOutputDevice(){
+        return currentOutputDevice != nullptr;
+    }
+};
+
+DeviceManager* DeviceManager:: instance = nullptr;
