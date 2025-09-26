92537927fc341c64d85b45aeb228b82d5eea57cf
Divyansh Sharma <108236157+cusp13@users.noreply.github.com>
2025-09-26 09:03:59 +0530

    Add files via upload

---
+++ Food-delivery-app/factories/NowOrderFactory.h
@@ -0,0 +1,33 @@
+#pragma once
+#include<iostream>
+#include<string>
+#include "OrderFactory.h"
+#include "../models/DeliveryOrder.h"
+#include "../models/PickUp.h"
+#include "../utils/TimeUtils.h"
+using namespace std;
+
+class NowOrderFactory : public OrderFactory {
+public:
+   Order* createOrder(User* user, Cart* cart, Restaurant* restaurant, const vector<MenuItem>& menuItems, 
+        PaymentStrategy* paymentStrategy, double totalCost, const string& orderType) override {
+        Order* order = nullptr;
+        if(orderType == "Delivery") {
+            auto deliveryOrder = new DeliveryOrder();
+            deliveryOrder->setUserAddress(user->getAddress());
+            order = deliveryOrder;
+        } else if(orderType == "PickUpOrder") {
+            auto pickUp = new PickUpOrder();
+            pickUp->setRestaurantAddress(user->getAddress());
+            order = pickUp;
+        }
+        order->setUser(user);
+        order->setRestaurant(restaurant);
+        order->setItems(menuItems);
+        order->setPaymentStrategy(paymentStrategy);
+        order->setScheduled(TimeUtils::getCurrentTime());
+        order->setTotal(totalCost);
+
+        return order;
+    }
+};
