1e3e91ad5f987b1858e0df4bcc1758c956fe1db9
Divyansh Sharma <108236157+cusp13@users.noreply.github.com>
2025-09-26 09:08:49 +0530

    Create PaymentStrategy.h

---
+++ Food-delivery-app/strategy/PaymentStrategy.h
@@ -0,0 +1,10 @@
+#pragma once
+#include<iostream>
+#include<string>
+using namespace std;
+
+class PaymentStrategy {
+public:
+    virtual void pay(double amount) = 0;
+    virtual ~PaymentStrategy() {};
+};
